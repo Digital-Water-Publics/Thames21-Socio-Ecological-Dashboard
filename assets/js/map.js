1ca3c65d0cf34c5569a675c6e128b04d19493256
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-12-16 01:52:19 +0000

    update map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -162,31 +162,12 @@ window.onload = function () {
 
 
     map.on('click', 'mc_value', (e) => {
-
-        var group = e.features[0].properties.group 
-
-        if(group == 1){
+        // Copy coordinates array.
         new mapboxgl.Popup()
             .setLngLat(e.lngLat)
-            .setHTML('<div id="bg" style="height:100%;width:100%;background-color:#b92727;"> <p> Management catchment: ' + e.features[0].properties.MC + "</br> <p> Waterbody sentiment score: " + parseFloat(e.features[0].properties.senticent_polarity).toFixed(4)) 
-            .addTo(map);    
-        }
-        
-        
-        if(group == 2){
-            new mapboxgl.Popup()
-                .setLngLat(e.lngLat)
-                .setHTML('<div id="bg" style="height:100%;width:100%;background-color:#f2ee0f;"> <p> Management catchment: ' + e.features[0].properties.MC + "</br> <p> Waterbody sentiment score: " + parseFloat(e.features[0].properties.senticent_polarity).toFixed(4)) 
-                .addTo(map);    
-            }
+            .setHTML('<p> Managment catchment: ' + e.features[0].properties.MC + "</br> <p> Waterbody sentiment score: " + parseFloat(e.features[0].properties.senticent_polarity).toFixed(4))
+            .addTo(map);
 
-            
-        if(group == 3){
-            new mapboxgl.Popup()
-                .setLngLat(e.lngLat)
-                .setHTML('<div id="bg" style="height:100%;width:100%;background-color:#1a8812;"> <p> Management catchment: ' + e.features[0].properties.MC + "</br> <p> Waterbody sentiment score: " + parseFloat(e.features[0].properties.senticent_polarity).toFixed(4)) 
-                .addTo(map);    
-            }
     });
 
     map.on('click', 'oc', (e) => {
@@ -237,7 +218,7 @@ window.onload = function () {
     map.addControl(new mapboxgl.NavigationControl());
 
     document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
-    
+
     const ocLegendEl = document.getElementById('oc-legend');
     const mcLegendEl = document.getElementById('mc-legend');
     const riverLegendEl = document.getElementById('wb-legend');
