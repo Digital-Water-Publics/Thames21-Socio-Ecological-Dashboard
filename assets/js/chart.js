2fefc7ca39f9755eb70c3ab82c246a84c483a302
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2022-04-11 23:35:32 +0100

    update dash

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -1,4 +1,48 @@
-function chart(mean_senti_path) {
+function chart(mean_senti_path, eco,emo_freq_path,nounphrase_path) {
+
+    var tabulate = function (data,columns) {
+        var table = d3.select('#csv').append('table')
+          var thead = table.append('thead')
+          var tbody = table.append('tbody')
+      
+          thead.append('tr')
+            .selectAll('th')
+              .data(columns)
+              .enter()
+            .append('th')
+              .text(function (d) { return d })
+      
+          var rows = tbody.selectAll('tr')
+              .data(data)
+              .enter()
+            .append('tr')
+      
+          var cells = rows.selectAll('td')
+              .data(function(row) {
+                  return columns.map(function (column) {
+                      return { column: column, value: row[column] }
+                })
+            })
+            .enter()
+          .append('td')
+            .text(function (d) { return d.value })
+            .style('background-color', function(d) { 
+                if (d.value == "Poor") {return 'orange';}
+                else if (d.value == "Moderate") {return 'yellow';}
+                else if (d.value == "Good") {return 'green';}
+                else {return 'red';}
+              });
+      
+        return table;
+      }
+      
+      d3.csv(eco,function (data) {
+          var columns = ['2013','2014','2015','2016', '2019']
+        tabulate(data,columns)
+      })
+
+
+
      // set the dimensions and margins of the graph
      var margin = {
         top: 10,
@@ -28,8 +72,6 @@ d3.csv(mean_senti_path,
         }
     },
 
-    // Now I can use this dataset:
-    // Now I can use this dataset:
     function (data) {
 
         // Add X axis --> it is a date format
@@ -159,6 +201,131 @@ d3.csv(mean_senti_path,
                     })
                 )
         });
+
+           // set the dimensions and margins of the graph
+    var margin1 = {
+        top: 20,
+        right: 20,
+        bottom: 40,
+        left: 185
+    },
+    width1 = 550 - margin.left - margin.right,
+    height2 = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg1 = d3.select("#my_dataviz1")
+    .append("svg")
+    .attr("width", width1 + margin1.left + margin1.right)
+    .attr("height", height2 + margin1.top + margin1.bottom)
+    .append("g")
+    .attr("transform",
+        "translate(" + margin1.left + "," + margin1.top + ")");
+
+// Parse the Data
+d3.csv(emo_freq_path, function (data) {
+
+    // Add X axis
+    var x = d3.scaleLinear()
+        .domain([0, 50])
+        .range([0, width]);
+    svg1.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .style("stroke", "white")
+        .style("font-family", "'M PLUS Rounded 1c', sans-serif")
+        .style("font-size", "16px")
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end");
+
+    // Y axis
+    var y = d3.scaleBand()
+        .range([0, height])
+        .domain(data.map(function (d) {
+            return d.sentiment;
+        }))
+        .padding(.1);
+    svg1.append("g")
+        .style("stroke", "white")
+        .style("font-size", "16px")
+        .style("font-family", "'M PLUS Rounded 1c', sans-serif")
+        .call(d3.axisLeft(y))
+
+    //Bars
+    svg1.selectAll("myRect")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("x", x(0))
+        .attr("y", function (d) {
+            return y(d.sentiment);
+        })
+        .attr("width", function (d) {
+            return x(d.percent);
+        })
+        .attr("height", y.bandwidth())
+        .attr("fill", "white")
+
+})
+
+
+
+// set the dimensions and margins of the graph
+var margin5 = {
+        top: 20,
+        right: 45,
+        bottom: 20,
+        left: 20
+    },
+    width3 = 800 - margin.left - margin.right,
+    height3 = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg4 = d3.select("#my_dataviz4").append("svg")
+    .attr("width", width3 + margin5.left + margin5.right)
+    .attr("height", height3 + margin5.top + margin5.bottom)
+    .append("g");
+
+d3.csv(nounphrase_path, function (data) {
+
+
+    // Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
+    // Wordcloud features that are different from one word to the other must be here
+    var layout = d3.layout.cloud()
+        .size([width3, height3])
+        .words(data.map(function (d) {
+            return {
+                text: d.text
+            };
+        }))
+        .padding(5) //space between words
+        .rotate(0) // rotation angle in degrees
+        .fontSize(32) // font size of words
+        .on("end", draw);
+    layout.start();
+
+    // This function takes the output of 'layout' above and draw the words
+    // Wordcloud features that are THE SAME from one word to the other can be here
+    function draw(words) {
+        svg4
+            .append("g")
+            .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+            .selectAll("text")
+            .data(words)
+            .enter().append("text")
+            .style("font-size", "2em")
+            .style("fill", "white")
+            .attr("text-anchor", "middle")
+            .style("font-family", "'M PLUS Rounded 1c', sans-serif")
+            .attr("transform", function (d) {
+                return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+            })
+            .text(function (d) {
+                return d.text;
+            })
+    };
+
+});
 
     })
 }
--- assets/js/map.js
+++ assets/js/map.js
@@ -12,102 +12,6 @@ window.onload = function () {
 
     // Load map
     map.on('load', () => {
-        // map.addSource('rbd', {
-        //     'type': 'geojson',
-        //     'data': 'data/rbd_cat.geojson'
-        // });
-
-        // map.addLayer({
-        //     'id': 'rbd_value',
-        //     'source': 'rbd',
-        //     'maxzoom': 1,
-        //     'minzoom': 5,
-        //     'type': 'fill',
-        //     'paint': {
-        //         'fill-color': [
-        //             'interpolate',
-        //             ['linear'],
-        //             ['get', 'group'],
-        //             1,
-        //             '#b92727',
-        //             2,
-        //             '#f2ee0f',
-        //             3,
-        //             '#1a8812'
-        //         ],
-        //         'fill-opacity': 0.75
-        //     }
-        // }, );
-
-        map.addSource('mc', {
-            'type': 'geojson',
-            'data': 'data/mc_cat.geojson'
-        });
-
-        map.addLayer({
-            'id': 'mc_value',
-            'source': 'mc',
-            'maxzoom': 8,
-            'minzoom': 5,
-            'type': 'fill',
-            'paint': {
-                'fill-color': [
-                    'interpolate',
-                    ['linear'],
-                    ['get', 'group'],
-                    1,
-                    '#b92727',
-                    2,
-                    '#f2ee0f',
-                    3,
-                    '#1a8812'
-                ],
-                'fill-opacity': 0.75
-            }
-        }, );
-
-        // map.addLayer({
-        //     'id': 'outline',
-        //     'maxzoom':11,
-        //     'minzoom': 9.1,
-        //     'type': 'line',
-        //     'source': 'mc',
-        //     'layout': {},
-        //     'paint': {
-        //         'line-color': '#000',
-        //         'line-width': 3
-        //     }
-        // });
-
-        map.addSource('oc', {
-            'type': 'geojson',
-            'data': 'data/oc_cat.geojson'
-        });
-
-        map.addLayer({
-            'maxzoom': 11,
-            'minzoom': 8,
-            'id': 'oc',
-            'source': 'oc',
-            'type': 'fill',
-            // only include features for which the "isCounty"
-            // property is "true"
-            'paint': {
-                'fill-color': [
-                    'interpolate',
-                    ['linear'],
-                    ['get', 'group'],
-                    1,
-                    '#b92727',
-                    2,
-                    '#f2ee0f',
-                    3,
-                    '#1a8812'
-                ],
-
-                'fill-opacity': 0.75
-            }
-        });
 
 
         map.addSource('wb', {
@@ -118,8 +22,6 @@ window.onload = function () {
         map.addLayer({
             'id': 'wb_value',
             'source': 'wb',
-            'maxzoom': 5,
-            'minzoom': 20,
             'type': 'line',
             'paint': {
                 'line-width': 5,
@@ -151,7 +53,7 @@ window.onload = function () {
                 // Add a tree emoji as a prefix for custom
                 // data results using carmen geojson format:
                 // https://github.com/mapbox/carmen/blob/master/carmen-geojson.md
-                feature['place_name'] = `🌊  ${feature.properties.Name}`;
+                feature['place_name'] = `${feature.properties.Name}`;
                 feature['center'] = feature.geometry.coordinates;
               
                 matchingFeatures.push(feature);
@@ -160,24 +62,6 @@ window.onload = function () {
         return matchingFeatures;
     }
 
-
-
-    map.on('click', 'mc_value', (e) => {
-        // Copy coordinates array.
-        new mapboxgl.Popup()
-            .setLngLat(e.lngLat)
-            .setHTML('<p> Managment catchment: ' + e.features[0].properties.MC + "</br> <p> Waterbody sentiment score: " + parseFloat(e.features[0].properties.senticent_polarity).toFixed(4))
-            .addTo(map);
-
-    });
-
-    map.on('click', 'oc', (e) => {
-        // Copy coordinates array.
-        new mapboxgl.Popup()
-            .setLngLat(e.lngLat)
-            .setHTML('<p> Operational catchment: ' + e.features[0].properties.OC + "</br> <p> Waterbody sentiment score: " + parseFloat(e.features[0].properties.senticent_polarity).toFixed(4))
-            .addTo(map);
-    });
 
     map.on('click', 'wb_value', (e) => {
 
@@ -188,27 +72,10 @@ window.onload = function () {
             .addTo(map);
     });
 
-    map.on('mouseenter', 'oc', () => {
-        map.getCanvas().style.cursor = 'pointer';
-    });
 
-    // Change it back to a pointer when it leaves.
-    map.on('mouseleave', 'oc', () => {
-        map.getCanvas().style.cursor = '';
-    });
 
-    map.on('mouseenter', 'mc', () => {
-        map.getCanvas().style.cursor = 'pointer';
-    });
 
-    // Change it back to a pointer when it leaves.
-    map.on('mouseleave', 'mc', () => {
-        map.getCanvas().style.cursor = '';
-    });
 
-    // map.on('hover','mc', (e) => {
-
-    // })
     const geocoder = new MapboxGeocoder({
         accessToken: mapboxgl.accessToken,
         mapboxgl: mapboxgl,
@@ -221,53 +88,28 @@ window.onload = function () {
 
     document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
 
-    const ocLegendEl = document.getElementById('oc-legend');
-    const mcLegendEl = document.getElementById('mc-legend');
-    const riverLegendEl = document.getElementById('wb-legend');
-
-    
+  
     geocoder.on('result', (e) => {
-        d3.selectAll("#my_dataviz > *").remove();
         let WBID_Select = e.result.properties.WBID;
+        let Name_Select = e.result.properties.Name;
+ 
+
+        d3.selectAll("#my_dataviz > *").remove();
+        d3.selectAll("#my_dataviz1 > *").remove();
+        d3.selectAll("#my_dataviz2 > *").remove();
+        d3.selectAll("#my_dataviz4 > *").remove();
+
+        document.getElementById("scale").innerHTML = "You are viewing the latest data for: " + Name_Select;
 
         var eco = "https://raw.githubusercontent.com/Digital-Water-Publics/Thames21-Socio-Ecological-Data/main/pot-mi/Open-Data/Thames/" + WBID_Select + "/eco-class.csv" 
-        var senti = "https://raw.githubusercontent.com/Digital-Water-Publics/Thames21-Socio-Ecological-Data/main/pot-mi/Open-Data/Thames/" + WBID_Select + "/emolex-frequency.csv" 
+        var emo_freq_path = "https://raw.githubusercontent.com/Digital-Water-Publics/Thames21-Socio-Ecological-Data/main/pot-mi/Open-Data/Thames/" + WBID_Select + "/emolex-frequency.csv" 
         var rnag = "https://raw.githubusercontent.com/Digital-Water-Publics/Thames21-Socio-Ecological-Data/main/pot-mi/Open-Data/Thames/" + WBID_Select + "/rnag.csv" 
-         var noun = "https://raw.githubusercontent.com/Digital-Water-Publics/Thames21-Socio-Ecological-Data/main/pot-mi/Open-Data/Thames/" + WBID_Select + "/common-nounphrase.csv" 
+         var nounphrase_path = "https://raw.githubusercontent.com/Digital-Water-Publics/Thames21-Socio-Ecological-Data/main/pot-mi/Open-Data/Thames/" + WBID_Select + "/common-nounphrase.csv" 
 
          var polarity = "https://raw.githubusercontent.com/Digital-Water-Publics/Thames21-Socio-Ecological-Data/main/pot-mi/Open-Data/Thames/" + WBID_Select + "/polarity-score.csv" 
 
-         chart(polarity)
+         chart(polarity,eco,emo_freq_path,nounphrase_path)
 
         });
 
-        geocoder.on('clear', () => {
-            results.innerText = '';
-            });
-
-    map.on('zoom', () => {
-
-        if (map.getZoom() < 8) {
-            ocLegendEl.style.display = 'none';
-            riverLegendEl.style.display = 'none';
-            mcLegendEl.style.display = 'block';
-            document.getElementById("scale").innerHTML = "You are viewing at the Management Catchment scale"
-        }
-
-
-        if (map.getZoom() < 11 && map.getZoom() > 8) {
-            ocLegendEl.style.display = 'block';
-            mcLegendEl.style.display = 'none';
-            riverLegendEl.style.display = 'none';
-            document.getElementById("scale").innerHTML = "You are viewing at the Operational Catchment scale"
-        }
-
-        if (map.getZoom() > 11) {
-            ocLegendEl.style.display = 'none';
-            riverLegendEl.style.display = 'block';
-            mcLegendEl.style.display = 'none';
-            document.getElementById("scale").innerHTML = "You are viewing at the Waterbody scale"
-        }
-
-    });
 };
