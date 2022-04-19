66afc94957003801c7d6aad688e6bebc467404b4
Nathanael <nathanaelsheehan@gmail.com>
2022-04-19 01:24:29 +0100

    new functions bb

--- example/js/chart.js
+++ example/js/chart.js
@@ -6,8 +6,13 @@
  * @param {string} nounphrase_path 
  */
 
- function chart(mean_senti_path, eco, emo_freq_path, nounphrase_path) {
+function chart(mean_senti_path, eco, emo_freq_path, nounphrase_path) {
 
+    /**
+     * 
+     * Ecological status csv
+     * 
+     */
     var tabulate = function (data, columns) {
         var table = d3.select('#csv').append('table')
         var thead = table.append('thead')
@@ -61,26 +66,30 @@
         tabulate(data, columns)
     })
 
-
+    /**
+     * 
+     * Sentiment Polarity Score
+     * 
+     */
 
     // set the dimensions and margins of the graph
-    var margin = {
+    var margin0 = {
             top: 10,
             right: 30,
             bottom: 30,
             left: 60
         },
-        width = 1200 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
+        width0 = 1200 - margin0.left - margin0.right,
+        height0 = 400 - margin0.top - margin0.bottom;
 
     // append the svg object to the body of the page
-    var svg = d3.select("#my_dataviz")
+    var svg0 = d3.select("#my_dataviz")
         .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
+        .attr("width", width0 + margin0.left + margin0.right)
+        .attr("height", height0 + margin0.top + margin0.bottom)
         .append("g")
         .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
+            "translate(" + margin0.left + "," + margin0.top + ")");
 
     //Read the data for graph
     d3.csv(mean_senti_path,
@@ -99,12 +108,12 @@
                 .domain(d3.extent(data, function (d) {
                     return d.date;
                 }))
-                .range([0, width]);
-            xAxis = svg.append("g")
+                .range([0, width0]);
+            xAxis = svg0.append("g")
                 .style("stroke", "white")
                 .style("font-size", "16px")
                 .style("font-family", "'M PLUS Rounded 1c', sans-serif")
-                .attr("transform", "translate(0," + height + ")")
+                .attr("transform", "translate(0," + height0 + ")")
                 .call(d3.axisBottom(x));
 
             // Add Y axis
@@ -112,19 +121,19 @@
                 .domain([-5, d3.max(data, function (d) {
                     return +d.value;
                 })])
-                .range([height, 0]);
-            yAxis = svg.append("g")
+                .range([height0, 0]);
+            yAxis = svg0.append("g")
                 .style("stroke", "white")
                 .style("font-family", "'M PLUS Rounded 1c', sans-serif")
                 .style("font-size", "16px")
                 .call(d3.axisLeft(y));
 
             // Add a clipPath: everything out of this area won't be drawn.
-            var clip = svg.append("defs").append("svg:clipPath")
+            var clip = svg0.append("defs").append("svg:clipPath")
                 .attr("id", "clip")
                 .append("svg:rect")
-                .attr("width", width)
-                .attr("height", height)
+                .attr("width", width0)
+                .attr("height", height0)
                 .attr("x", 0)
                 .attr("y", 0);
 
@@ -132,13 +141,13 @@
             var brush = d3.brushX() // Add the brush feature using the d3.brush function
                 .extent([
                     [0, 0],
-                    [width, height]
+                    [width0, height0]
                 ]) // initialise the brush area: start at 0,0 and finishes at width,height: it means I select the whole graph area
                 .on("end",
                     updateChart) // Each time the brush selection changes, trigger the 'updateChart' function
 
             // Create the line variable: where both the line and the brush take place
-            var line = svg.append('g')
+            var line = svg0.append('g')
                 .attr("clip-path", "url(#clip)")
 
             // Add the line
@@ -204,7 +213,7 @@
             }
 
             // If user double click, reinitialize the chart
-            svg.on("dblclick", function () {
+            svg0.on("dblclick", function () {
                 x.domain(d3.extent(data, function (d) {
                     return d.date;
                 }))
@@ -222,6 +231,149 @@
                     )
             });
 
+            /**
+             * 
+             * Reasons for not achieving good
+             * 
+             */
+             var units = "Widgets";
+
+             // set the dimensions and margins of the graph
+             var margin = {top: 10, right: 10, bottom: 10, left: 10},
+                 width = 700 - margin.left - margin.right,
+                 height = 300 - margin.top - margin.bottom;
+             
+             // format variables
+             var formatNumber = d3.format(",.0f"),    // zero decimal places
+                 format = function(d) { return formatNumber(d) + " " + units; },
+                 color = d3.scaleOrdinal(d3.schemeCategory10);
+             
+             // append the svg object to the body of the page
+             var svg = d3.select("body").append("svg")
+                 .attr("width", width + margin.left + margin.right)
+                 .attr("height", height + margin.top + margin.bottom)
+               .append("g")
+                 .attr("transform", 
+                       "translate(" + margin.left + "," + margin.top + ")");
+             
+             // Set the sankey diagram properties
+             var sankey = d3.sankey()
+                 .nodeWidth(36)
+                 .nodePadding(40)
+                 .size([width, height]);
+             
+             var path = sankey.link();
+             
+             // load the data
+             d3.csv("sankey.csv", function(error, data) {
+              
+               //set up graph in same style as original example but empty
+               graph = {"nodes" : [], "links" : []};
+             
+               data.forEach(function (d) {
+                 graph.nodes.push({ "name": d.source });
+                 graph.nodes.push({ "name": d.target });
+                 graph.links.push({ "source": d.source,
+                                    "target": d.target,
+                                    "value": +d.value });
+                });
+             
+               // return only the distinct / unique nodes
+               graph.nodes = d3.keys(d3.nest()
+                 .key(function (d) { return d.name; })
+                 .object(graph.nodes));
+             
+               // loop through each link replacing the text with its index from node
+               graph.links.forEach(function (d, i) {
+                 graph.links[i].source = graph.nodes.indexOf(graph.links[i].source);
+                 graph.links[i].target = graph.nodes.indexOf(graph.links[i].target);
+               });
+             
+               // now loop through each nodes to make nodes an array of objects
+               // rather than an array of strings
+               graph.nodes.forEach(function (d, i) {
+                 graph.nodes[i] = { "name": d };
+               });
+             
+               sankey
+                   .nodes(graph.nodes)
+                   .links(graph.links)
+                   .layout(32);
+             
+               // add in the links
+               var link = svg.append("g").selectAll(".link")
+                   .data(graph.links)
+                 .enter().append("path")
+                   .attr("class", "link")
+                   .attr("d", path)
+                   .style("stroke-width", function(d) { return Math.max(1, d.dy); })
+                   .sort(function(a, b) { return b.dy - a.dy; });
+             
+               // add the link titles
+               link.append("title")
+                     .text(function(d) {
+                         return d.source.name + " → " + 
+                             d.target.name + "\n" + format(d.value); });
+             
+               // add in the nodes
+               var node = svg.append("g").selectAll(".node")
+                   .data(graph.nodes)
+                 .enter().append("g")
+                   .attr("class", "node")
+                   .attr("transform", function(d) { 
+                       return "translate(" + d.x + "," + d.y + ")"; })
+                   .call(d3.drag()
+                     .subject(function(d) {
+                       return d;
+                     })
+                     .on("start", function() {
+                       this.parentNode.appendChild(this);
+                     })
+                     .on("drag", dragmove));
+             
+               // add the rectangles for the nodes
+               node.append("rect")
+                   .attr("height", function(d) { return d.dy; })
+                   .attr("width", sankey.nodeWidth())
+                   .style("fill", function(d) { 
+                       return d.color = color(d.name.replace(/ .*/, "")); })
+                   .style("stroke", function(d) { 
+                       return d3.rgb(d.color).darker(2); })
+                 .append("title")
+                   .text(function(d) { 
+                       return d.name + "\n" + format(d.value); });
+             
+               // add in the title for the nodes
+               node.append("text")
+                   .attr("x", -6)
+                   .attr("y", function(d) { return d.dy / 2; })
+                   .attr("dy", ".35em")
+                   .attr("text-anchor", "end")
+                   .attr("transform", null)
+                   .text(function(d) { return d.name; })
+                 .filter(function(d) { return d.x < width / 2; })
+                   .attr("x", 6 + sankey.nodeWidth())
+                   .attr("text-anchor", "start");
+             
+               // the function for moving the nodes
+               function dragmove(d) {
+                 d3.select(this)
+                   .attr("transform", 
+                         "translate(" 
+                            + d.x + "," 
+                            + (d.y = Math.max(
+                               0, Math.min(height - d.dy, d3.event.y))
+                              ) + ")");
+                 sankey.relayout();
+                 link.attr("d", path);
+               }
+             });
+
+            /**
+             * 
+             * Emmotional Classifcation Frequency
+             * 
+             */
             // set the dimensions and margins of the graph
             var margin1 = {
                     top: 20,
@@ -288,7 +440,11 @@
 
             })
 
-
+            /**
+             * 
+             * Common nounphrases word cloud
+             * 
+             */
 
             // set the dimensions and margins of the graph
             var margin5 = {
